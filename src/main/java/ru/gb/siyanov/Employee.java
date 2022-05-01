445dbbfb48be1eaa1d1d5fee96ad344ac5d241c8
Viscount <siyanov.d@sar.garm.ru>
2022-05-01 21:16:23 +0400

    start homework 5

---
+++ src/main/java/ru/gb/siyanov/Employee.java
@@ -0,0 +1,34 @@
+package ru.gb.siyanov;
+
+public class Employee {
+    String firstName;
+    String lastName;
+    String Position;
+    String email;
+    String telephone;
+    long salary;
+    int age;
+
+
+
+    public static void main(String[] args) {
+        String a;
+        String b;
+        String c;
+        String d;
+        String e;
+        long f;
+        int g;
+        public static void InformationAboutObject( a, b, c, d, e, f, g){
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+            System.out.println("c = " + c);
+            System.out.println("d = " + d);
+            System.out.println("e = " + e);
+            System.out.println("f = " + f);
+            System.out.println("g = " + g);
+        }
+    }
+
+
+}
--- src/main/java/ru/gb/siyanov/HomeWorkApp.java
+++ src/main/java/ru/gb/siyanov/HomeWorkApp.java
@@ -1,17 +1,9 @@
 package ru.gb.siyanov;
 
 public class HomeWorkApp {
-
-
-
-
-
-
-
-
-
-
     public static void main(String[] args) {
+        Employee emp1 = new Employee();
+
 
     }
 }
