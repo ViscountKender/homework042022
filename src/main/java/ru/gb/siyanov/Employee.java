750321a88d77e59a845a025aaebae8851b22e4ec
Viscount <siyanov.d@sar.garm.ru>
2022-05-02 20:19:01 +0400

    finish homework 5

--- src/main/java/ru/gb/siyanov/Employee.java
+++ src/main/java/ru/gb/siyanov/Employee.java
@@ -1,34 +1,99 @@
 package ru.gb.siyanov;
 
 public class Employee {
-    String firstName;
-    String lastName;
-    String Position;
-    String email;
-    String telephone;
-    long salary;
-    int age;
+    private String firstName;
+    private String lastName;
+    private String Position;
+    private String email;
+    private String phone;
+    private long salary;
+    private int age;
+    public Employee(String firstName,String lastName,String Position,String email,String phone,long salary,int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.Position = Position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return Position;
+    }
+
+    public void setPosition(String position) {
+        Position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 0 && age < 150){
+            this.age = age;
+        }
+
+    }
 
     public static void main(String[] args) {
-        String a;
-        String b;
-        String c;
-        String d;
-        String e;
-        long f;
-        int g;
-        public static void InformationAboutObject( a, b, c, d, e, f, g){
-            System.out.println("a = " + a);
-            System.out.println("b = " + b);
-            System.out.println("c = " + c);
-            System.out.println("d = " + d);
-            System.out.println("e = " + e);
-            System.out.println("f = " + f);
-            System.out.println("g = " + g);
+//        String a;
+//        String b;
+//        String c;
+//        String d;
+//        String e;
+//        long f;
+//        int g;
+//        InformationAboutObject( a, b, c, d, e,f,g);
         }
-    }
+
+//    public static void    InformationAboutObject() {
+//        System.out.println("a = " + a);
+//        System.out.println("b = " + b);
+//        System.out.println("c = " + c);
+//        System.out.println("d = " + d);
+//        System.out.println("e = " + e);
+//        System.out.println("f = " + f);
+//        System.out.println("g = " + g);
+//    }
+
 
 
 }
--- src/main/java/ru/gb/siyanov/HomeWorkApp.java
+++ src/main/java/ru/gb/siyanov/HomeWorkApp.java
@@ -2,7 +2,24 @@ package ru.gb.siyanov;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        Employee emp1 = new Employee();
+        Employee [] arrayEmployee = new Employee[5];
+        arrayEmployee[0] = new Employee("Иван", "Иванов","Директор","q@mail.ru","+7111111",100000,50);
+        arrayEmployee[1] = new Employee("Петр","Петров","Бухгалтер","w@mail.ru","+7222222",50000,40);
+        arrayEmployee[2] = new Employee("Анна", "Рыдаева","Менеджер","e@mail.ru","+7333333",35000,35);
+        arrayEmployee[3] = new Employee("Буян","Вакутагин","Водитель","r@mail.ru","+7444444",25000,20);
+        arrayEmployee[4] = new Employee("Леонид","Тюк","Охранник","t@mail.ru","+7555555",7000,55);
+        for (int i = 0; i < arrayEmployee.length; i++) {
+            //if(arrayEmployee[i].getAge() >40){
+                System.out.println(arrayEmployee[i]);
+            }
+
+
+
+
+
+
+
+
 
 
     }
